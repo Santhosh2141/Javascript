82dbc17abaaf55db28cbae431b9c8f1fcb0e95e6
Santhosh2141 <snthshsrinivas@gmail.com>
2025-01-06 16:06:03 +0530

    added all products and updated the view

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,37 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090 
-},{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size BasketBall',
-  rating: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095 
-},{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799 
-},{
-  image: 'images/products/bathroom-rug.jpg',
-  name: 'Bathroom Rug',
-  rating: {
-    stars: 3.0,
-    count: 104
-  },
-  priceCents: 305 
-}];
-
 let productsHtml = '';
 products.forEach((product)=>{
   productsHtml += `
