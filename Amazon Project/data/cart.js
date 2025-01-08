096428a95c06ee8194dabc43c46b95ff2dc35e5a
Santhosh2141 <snthshsrinivas@gmail.com>
2025-01-08 21:45:14 +0530

    made cart update after refreshing, added delete cart function

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -1,13 +1,19 @@
-export const cart  =[{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 3
-},{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 2
-}]; //export allows using this in other files
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart){
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 3
+  },{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }]; //export allows using this in other files
+} 
+console.log(cart);
 
 //moving all cart related code here
-
+export function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId){
   let selectedValue = document.querySelector(`.js-selected-value-${productId}`);
   const quantity = Number(selectedValue.value);
@@ -25,4 +31,16 @@ export function addToCart(productId){
       quantity
     });
   }
+  saveToStorage();
+}
+
+export function removeFromCart(productId){
+  let newCart = [];
+  cart.forEach((cartItem)=>{
+    if (cartItem.productId !== productId){
+      newCart.push(cartItem);
+    };
+  });
+  cart = newCart;
+  saveToStorage();
 }
