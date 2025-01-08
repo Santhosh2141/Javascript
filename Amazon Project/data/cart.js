304933f904a9059afbf35bc38c1feb4d91b0196e
Santhosh2141 <snthshsrinivas@gmail.com>
2025-01-08 14:27:20 +0530

    updated checkout page for correct format

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -1,4 +1,10 @@
-export const cart  =[]; //export allows using this in other files
+export const cart  =[{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 3
+},{
+  productId: '5b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 2
+}]; //export allows using this in other files
 
 //moving all cart related code here
 
