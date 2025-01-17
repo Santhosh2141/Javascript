519e41b7562a82158a030aa70728aa21d2a4ac87
Santhosh2141 <snthshsrinivas@gmail.com>
2025-01-17 19:49:43 +0530

    converted products into Class

--- Amazon Project/data/products.js
+++ Amazon Project/data/products.js
@@ -1,4 +1,48 @@
 import { cart } from "./cart.js";
+import formatCurrency from "../scripts/utils/money.js";
+
+class Products {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  };
+
+  getStarsURL() {
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  };
+
+  getPrice() {
+     return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+const product1 = new Products({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+});
+console.log(product1);
 
 export function getProduct(productId){
   let matchingProduct;
@@ -9,6 +53,7 @@ export function getProduct(productId){
   });
   return matchingProduct;
 }
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -683,4 +728,9 @@ export const products = [
       "bag",
     ]
   }
-];
+].map((product) => {
+  return new Products(product);
+});
+
+console.log(products);
+//map creates a new array. when we return each element will go inside that array
