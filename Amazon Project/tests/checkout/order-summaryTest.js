86f83ab5feacb3954af35b2c5a1864021375b807
Santhosh2141 <snthshsrinivas@gmail.com>
2025-01-20 20:49:30 +0530

    used backed to load products and perform tests

--- Amazon Project/tests/checkout/order-summaryTest.js
+++ Amazon Project/tests/checkout/order-summaryTest.js
@@ -1,8 +1,15 @@
 import { renderOrderSumamry } from "../../scripts/checkout/order-summary.js";
 import { cart, loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('Test suite: Render Order Summary', () => {
 
+  // done waits till loadproducts is complete. so we need to call done to go to next step
+  beforeAll((done)=>{
+    loadProducts(()=>{
+      done();
+    });
+  })
   beforeEach (() => {
     spyOn(localStorage, 'setItem');
 
@@ -50,13 +57,13 @@ describe('Test suite: Render Order Summary', () => {
 
     document.querySelector('.js-delete-link-e43638ce-6aa0-4b85-b27f-e1d07eb678c6').click();
     expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
-
+    console.log(cart);
     expect(document.querySelector('.js-cart-container-e43638ce-6aa0-4b85-b27f-e1d07eb678c6')).toEqual(null);
 
     expect(document.querySelector('.js-cart-container-15b6fc6f-327a-4ec4-896f-486349e85a3d')).not.toEqual(null);
 
-    expect(cart.length).toEqual(1);
-    expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
   });
 
@@ -65,7 +72,7 @@ describe('Test suite: Render Order Summary', () => {
     expect(document.querySelector('.js-delivery-option-input-e43638ce-6aa0-4b85-b27f-e1d07eb678c6-3').checked).toEqual(true);
     expect(cart.length).toEqual(2);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(cart[0].deliveryOptionId).toEqual('1');
     expect(document.querySelector('.js-shipping-e43638ce-6aa0-4b85-b27f-e1d07eb678c6').innerHTML).toContain('$14.98');
     expect(document.querySelector('.js-total-e43638ce-6aa0-4b85-b27f-e1d07eb678c6').innerHTML).toContain('$75.49');
   })
